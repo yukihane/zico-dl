a1de943881857e288c9ae50fc26598cad74b7df9
yukihane <yukihane.feather@gmail.com>
2024-04-05 04:08:15 +0900

    ダウンロード対象を特定する関数

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+pub fn find_dl_target(url: &str) -> String {
+    url.to_string()
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -5,6 +9,16 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn find_dl_target_test() {
+        let result =
+            find_dl_target("https://www.dlsite.com/maniax/work/=/product_id/RJ292145.html");
+        assert_eq!(
+            result,
+            "https://trial.dlsite.com/doujin/RJ293000/RJ292145_trial.zip"
+        )
+    }
 
     #[test]
     fn it_works() {
