a01ffc2ed09ba8bb7070d4ec719d7fbdfbcc0e81
yukihane <yukihane.feather@gmail.com>
2024-04-05 05:05:14 +0900

    reqwestでの取得と非同期テストコード

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
-pub fn find_dl_target(url: &str) -> String {
-    url.to_string()
+pub async fn find_dl_target(url: &str) -> Result<String, reqwest::Error> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {body:?}");
+    Ok(url.to_string())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::future::IntoFuture;
+
     use super::*;
 
-    #[test]
-    fn find_dl_target_test() {
+    #[tokio::test]
+    async fn find_dl_target_test() {
         let result =
             find_dl_target("https://www.dlsite.com/maniax/work/=/product_id/RJ292145.html");
         assert_eq!(
-            result,
+            result.into_future().await.unwrap(),
             "https://trial.dlsite.com/doujin/RJ293000/RJ292145_trial.zip"
         )
     }
