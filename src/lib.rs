9a2dc4980d27354aae440ec6ae60d798522b689d
yukihane <yukihane.feather@gmail.com>
2024-04-06 21:12:39 +0900

    修正

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,17 @@ pub async fn find_dl_target(url: &str) -> Result<String, reqwest::Error> {
 
 #[cfg(test)]
 mod tests {
-    use std::future::IntoFuture;
 
     use super::*;
 
     #[tokio::test]
     async fn find_dl_target_test() {
         let result =
-            find_dl_target("https://www.dlsite.com/maniax/work/=/product_id/RJ292145.html");
+            find_dl_target("https://www.dlsite.com/maniax/work/=/product_id/RJ292145.html")
+                .await
+                .unwrap();
         assert_eq!(
-            result.into_future().await.unwrap(),
+            result,
             "https://trial.dlsite.com/doujin/RJ293000/RJ292145_trial.zip"
         )
     }
